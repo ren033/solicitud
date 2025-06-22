915ca4b9bd053bf553f0379a18176215185d46c9
ren033 <107058457+ren033@users.noreply.github.com>
2025-06-22 17:42:53 -0300

    test cliente

--- src/test/java/com/proyecto/solicitud/service/ClienteServiceTest.java
+++ src/test/java/com/proyecto/solicitud/service/ClienteServiceTest.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,23 +31,23 @@ class ClienteServiceTest
     }
 
     @Test
-    void testSaveCliente() {
-        Cliente cliente = new Cliente();
-        Cliente clienteSaved = new Cliente();
-        
-        when(clienteRepository.save(cliente)).thenReturn(clienteSaved);
+    public void testSaveCliente() {
+        Cliente cliente = new Cliente(1, "pass", "usercarlos",
+            "Carlos","carlos@gmail.com", "Calle 23", true);
 
-        Cliente resultado = clienteService.save(cliente);
-        assertThat(resultado.getId()).isEqualTo(1);
-        assertThat(resultado.getNombre()).isEqualTo("");
-        verify(clienteRepository).save(cliente);
+        when(clienteRepository.save(cliente)).thenReturn(cliente);
+        Cliente saved = clienteService.save(cliente);
+
+        assertNotNull(saved);
+        assertEquals(1, saved.getId());
+        assertEquals("Carlos", saved.getNombre());
     }
 
     @Test
     void testListClientes() {
-        Cliente c1 = new Cliente();
-        Cliente c2 = new Cliente();
-        Cliente c3 = new Cliente();
+        Cliente c1 = new Cliente(1, "pass", "usercarlos", "Carlos", "carlos@gmail.com", "Calle 23", true);
+        Cliente c2 = new Cliente(2, "passw0rd", "anonimo", "Antonio", "anon@gmail.com", "Edificio A2", true);
+        Cliente c3 = new Cliente(3, "321", "marii", "Mari", "marimail@email.com", "Dpto 10", false);
 
         when(clienteRepository.findAll()).thenReturn(Arrays.asList(c1, c2, c3));
 
