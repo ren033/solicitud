a3e4089529faebdcf8ddde4e35823767005f9c15
ren033 <107058457+ren033@users.noreply.github.com>
2025-06-30 18:26:48 -0300

    commit

--- src/main/java/com/proyecto/solicitud/controller/SolicitudControllerV2.java
+++ src/main/java/com/proyecto/solicitud/controller/SolicitudControllerV2.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +21,7 @@ import com.proyecto.solicitud.service.SolicitudService;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -28,6 +34,22 @@ public class SolicitudControllerV2
     @Autowired
     private SolicitudModelAssembler assembler;
 
+    @GetMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public CollectionModel<EntityModel<Solicitud>> getAll() {
+        List<EntityModel<Solicitud>> solicitudes = solicitudService.listSolicitudes().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(solicitudes,
+                linkTo(methodOn(SolicitudControllerV2.class).getAll()).withSelfRel());
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public EntityModel<Solicitud> getById(@PathVariable int id) {
+        Optional<Solicitud> solicitud = solicitudService.findById(id);
+        return assembler.toModel(solicitud);
+    }
+
     @GetMapping(value = "/cliente/{idCliente}", produces = MediaTypes.HAL_JSON_VALUE)
     public CollectionModel<EntityModel<Solicitud>> getByClienteId(@PathVariable Integer idCliente)
     {
@@ -36,5 +58,26 @@ public class SolicitudControllerV2
                 .collect(Collectors.toList());
         return CollectionModel.of(solicitudes,
         linkTo(methodOn(SolicitudControllerV2.class).getByClienteId(idCliente)).withSelfRel());
+    }
+
+    @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<Solicitud>> createSolicitud(@RequestBody Solicitud solicitud) {
+        Solicitud newSol = solicitudService.save(solicitud);
+        return ResponseEntity
+                .created(linkTo(methodOn(SolicitudControllerV2.class).getById(newSol.getId())).toUri())
+                .body(assembler.toModel(newSol));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<EntityModel<Solicitud>> update(@PathVariable int id, @RequestBody Solicitud solicitud) {
+        solicitud.setId(id);
+        Solicitud updatedSol = solicitudService.save(solicitud);
+        return ResponseEntity.ok(assembler.toModel(updatedSol));
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaTypes.HAL_JSON_VALUE)
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        solicitudService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
