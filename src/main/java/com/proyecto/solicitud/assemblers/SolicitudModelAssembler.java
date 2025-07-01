5c76576c8bb7fdab886a822a799c47e1a6efb15a
ren033 <re.barrientos@duocuc.cl>
2025-07-01 10:43:33 -0400

    commit

--- src/main/java/com/proyecto/solicitud/assemblers/SolicitudModelAssembler.java
+++ src/main/java/com/proyecto/solicitud/assemblers/SolicitudModelAssembler.java
@@ -3,7 +3,7 @@ package com.proyecto.solicitud.assemblers;
 
 import org.springframework.stereotype.Component;
 
-import com.proyecto.solicitud.controller.SolicitudController;
+import com.proyecto.solicitud.controller.SolicitudControllerV2;
 import com.proyecto.solicitud.model.Solicitud;
 
 import java.util.Optional;
@@ -17,12 +17,12 @@ public class SolicitudModelAssembler implements RepresentationModelAssembler<Sol
     @Override
     public EntityModel<Solicitud> toModel(Optional<Solicitud> solicitud) {
         return EntityModel.of(solicitud,
-                linkTo(methodOn(SolicitudController.class).getAll()).withRel("Solicitudes"));
+                linkTo(methodOn(SolicitudControllerV2.class).getAll()).withRel("Solicitudes"));
     }
     @Override
     public EntityModel<Solicitud> toModel(Solicitud solicitud) {
         return EntityModel.of(solicitud,
-                linkTo(methodOn(SolicitudController.class).getById(solicitud.getId())).withSelfRel());
+                linkTo(methodOn(SolicitudControllerV2.class).getById(solicitud.getId())).withSelfRel());
     }
 }
 
