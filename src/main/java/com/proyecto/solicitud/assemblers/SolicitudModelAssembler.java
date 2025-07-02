33dc2ab1ab9673cf7e5c82b0f044c18116b674a6
ren033 <107058457+ren033@users.noreply.github.com>
2025-07-02 12:01:07 -0300

    commit

--- src/main/java/com/proyecto/solicitud/assemblers/SolicitudModelAssembler.java
+++ src/main/java/com/proyecto/solicitud/assemblers/SolicitudModelAssembler.java
@@ -6,12 +6,9 @@ import org.springframework.stereotype.Component;
 import com.proyecto.solicitud.controller.SolicitudControllerV2;
 import com.proyecto.solicitud.model.Solicitud;
 
-<<<<<<< HEAD
-=======
 import java.util.Optional;
 
 import org.springframework.hateoas.EntityModel;
->>>>>>> 5c76576c8bb7fdab886a822a799c47e1a6efb15a
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
@@ -23,11 +20,6 @@ import org.springframework.hateoas.EntityModel;
 public class SolicitudModelAssembler implements RepresentationModelAssembler<Solicitud, EntityModel<Solicitud>>{
     @Override
     public EntityModel<Solicitud> toModel(Optional<Solicitud> solicitud) {
-<<<<<<< HEAD
-        return EntityModel.of(solicitud,
-                linkTo(methodOn(SolicitudController.class).getById(solicitud.getId())).withSelfRel(),
-                linkTo(methodOn(SolicitudController.class).getAll()).withRel("Solicitudes"));
-=======
         return EntityModel.of(solicitud,
                 linkTo(methodOn(SolicitudControllerV2.class).getAll()).withRel("Solicitudes"));
     }
@@ -35,7 +27,6 @@ public class SolicitudModelAssembler implements RepresentationModelAssembler<Sol
     public EntityModel<Solicitud> toModel(Solicitud solicitud) {
         return EntityModel.of(solicitud,
                 linkTo(methodOn(SolicitudControllerV2.class).getById(solicitud.getId())).withSelfRel());
->>>>>>> 5c76576c8bb7fdab886a822a799c47e1a6efb15a
     }
 }
 
